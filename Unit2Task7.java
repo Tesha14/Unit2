703efceed3c3c0e8e61ae9175c15a85e3377c98e
Tesha14 <Tesha105@yandex.ru>
2021-06-05 23:15:59 +0300

    Unit3

--- Unit2Task7.java
+++
@@ -1,9 +0,0 @@
-public class Unit2Task7 {
-    public static void main(String[] args) {
-        int d;
-        for (d = 1; d < 100; d+=2) {
-            System.out.println(d);
-
-        }
-    }
-}
